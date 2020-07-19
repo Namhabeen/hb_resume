f5befe9b9592be84c584917def451a06498fbe45
South_under_been <s2018s27@e-mirim.hs.kr>
2020-07-19 15:29:11 +0900

    modify navbar

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(window).scrollTop() > $("#about").height()){
+        if($(window).scrollTop() > $("#brace").height()){
             $(".navbar").css({"background-color":"yellow; width:100%; margin-left:0;"});   
         }
         else{
