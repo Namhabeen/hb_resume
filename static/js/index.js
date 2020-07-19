3c85e0e4205a02a8b2662aef27b05e82ffb272d3
South_under_been <s2018s27@e-mirim.hs.kr>
2020-07-19 15:35:51 +0900

    modify navbar

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > $("#jtitle").height()){
             $(".navbar").css({"background-color":"rgb(245, 221, 3)"});
-          
+            $(".navbar").css({"min-width":"100%"});   
+            $(".navbar").css({"margin-top":"0px"}); 
+            $(".nav-link").css({"color":"black"});
+            $(".navbar-brand").css({"color":"black"});  
         }
         else{
             $(".navbar").css({"background":"none"});
