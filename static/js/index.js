a3f5b296f939a0d55d0d9faf49c5542978c94b6b
South_under_been <s2018s27@e-mirim.hs.kr>
2020-07-20 10:40:12 +0900

    add loader

--- static/js/index.js
+++ static/js/index.js
@@ -17,3 +17,10 @@ $(document).ready(function(){
 
     })
 })
+
+var _showPage = function() {
+    var loader = $("div.loader");
+    var container = $("div.container");
+    loader.css("display","none");
+    container.css("display","block");
+  };
